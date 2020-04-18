427eec1ae50200e0ee0265fd01eac9479487eef8
zeleke <zzdd9251@gmail.com>
2020-04-18 14:06:43 -0700

    Updates

---
+++ precache-manifest.c3e3df1fcc3d4017bdb2bba23ac4d5da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215a76bccf1e01eed31cca319146bce6",
+    "url": "/randomquiz/index.html"
+  },
+  {
+    "revision": "aed2550a690714ba9927",
+    "url": "/randomquiz/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "9b680f870d877fa20646",
+    "url": "/randomquiz/static/js/2.084e664d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomquiz/static/js/2.084e664d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed2550a690714ba9927",
+    "url": "/randomquiz/static/js/main.63e15252.chunk.js"
+  },
+  {
+    "revision": "a2eba34b88323c1c972f",
+    "url": "/randomquiz/static/js/runtime-main.8aebbcf0.js"
+  }
+]);

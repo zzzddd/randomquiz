22f9d242256db98946c985af1e82a003eaeffb89
zeleke <zzdd9251@gmail.com>
2020-04-18 14:01:15 -0700

    deploy version added..

---
+++ build/precache-manifest.aee585cc1d36a18c53e165b57c5ee31f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19950f10078ac53476865ff47fcf06b9",
+    "url": "/quizbee/index.html"
+  },
+  {
+    "revision": "f3769f112bbace1b06ef",
+    "url": "/quizbee/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "aa90f1098797558a0d7f",
+    "url": "/quizbee/static/js/2.08a164c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizbee/static/js/2.08a164c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3769f112bbace1b06ef",
+    "url": "/quizbee/static/js/main.4f645b2b.chunk.js"
+  },
+  {
+    "revision": "42d4200f3e6f77acc426",
+    "url": "/quizbee/static/js/runtime-main.b1e78c3c.js"
+  }
+]);
